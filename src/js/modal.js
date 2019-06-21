52101ab07f47504c454e22cdd31256751972b736
bohdansavchuk <leonidovich1804@gmail.com>
2019-06-21 19:56:10 +0300

    animation and modal

---
+++ src/js/modal.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+
+  button.on('click', function(){
+    modal.addClass('modal_active');
+  });
+
+  close.on('click', function(){
+    modal.removeClass('modal_active');
+  });
+});
+
+$(document).ready(function(){
+  var button = $('#button-modal');
+  var modal = $('#success');
+  var close = $('#close-success');
+
+  button.on('click', function(){
+    modal.addClass('success_active');
+  });
+
+  close.on('click', function(){
+    modal.removeClass('success_active');
+  });
+});
